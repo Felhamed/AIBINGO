1c5348e3e3ffc590245fa60bd370a1f650510b41
Rubeshan <rubeshan@gmail.com>
2017-06-24 19:04:28 -0400

    orginized the js/html

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,66 @@
+var ctx1 = document.getElementById("donut");
+var myChart1 = new Chart(ctx1, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+      backgroundColor: [
+        "#2ecc71",
+        "#e74c3c"
+      ],
+      data: [75, 25]
+    }]
+  },
+  options: {
+    cutoutPercentage: 80,
+    rotation: 1*Math.PI,
+    circumference: 1*Math.PI
+  }
+});
+
+
+var ctx2 = document.getElementById("donut2");
+var myChart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [50, 50],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+    }]
+  },
+  options: {
+    cutoutPercentage: 80,
+    rotation: 1*Math.PI,
+    circumference: 1*Math.PI
+  }
+});
+
+var ctx3 = document.getElementById("donut3");
+var myChart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+      backgroundColor: [
+        "#2ecc71",
+        "#e74c3c"
+      ],
+      data: [30, 70]
+    }]
+  },
+  options: {
+    cutoutPercentage: 80,
+    rotation: 1*Math.PI,
+    circumference: 1*Math.PI
+  }
+});
+
 function showSpeed(data) {
   console.log(data);
   var speed = data.average_speed;
