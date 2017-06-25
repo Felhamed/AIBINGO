8da7a3e1ddf51859ca1cc6e4307ad4a292cc0244
Rubeshan <rubeshan@gmail.com>
2017-06-25 12:31:41 -0400

    changing some items in the humal detail page

--- src/js/app.js
+++ src/js/app.js
@@ -68,6 +68,40 @@ var myChart3 = new Chart(ctx3, {
   }
 });
 
+var myDoughnutChart = new Chart(ctx).Doughnut(data,option);
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+];
+
+$(function () {
+    $('.min-chart#chart-sales').easyPieChart({
+        barColor: "#4caf50",
+        onStep: function (from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+});
+
+
+
+
 function showSpeed(data) {
   console.log(data);
   var speed = data.average_speed;
